ca60fb8215e22c163988c390a8c540cc5021de48
Simon M <Simon M>
2019-04-29 15:16:29 +0200

    vec2Test added 2. try

--- source/vec2Test.cpp
+++ source/vec2Test.cpp
@@ -3,8 +3,9 @@
 int main (){
   
 // Vec2 a; // requires that 0.0 f == a.x and 0.0 f == a.y
-  std::cout << a.x << "\n" << a.y << "\n" <<"\n"
-  ;
+ // std::cout << "a.x" << "\n" << "a.y" << "\n"
+ //  <<"\n"
+ // ;
 /*
 Vec2 b; /*{5.1f, -9.3f};  requires that 5.1 f == Approx (b.x)
 and -9.3f == Approx (b.y)
